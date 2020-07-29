b176f46ffaab074d24bde42b646b1392e4880e99
Semen <semendubynin@gmail.com>
2020-07-29 11:29:43 +0300

    Initial Commit

---
+++ CoreDataDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataDemo
+//
+//  Created by Дубынин Семён on 29.07.2020.
+//  Copyright © 2020 Дубынин Семён. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
